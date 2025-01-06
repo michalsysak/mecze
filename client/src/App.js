09405809f35e8147387998376f0101c4242cbb35
Michuaee <michuaepl@gmail.com>
2025-01-06 11:42:38 +0100

    create

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Matches from './components/Matches';
+
+function App() {
+  return (
+      <div className="App">
+        <Matches />
+      </div>
+  );
+}
+
+export default App;
